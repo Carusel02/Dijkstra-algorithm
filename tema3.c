0da1970ee7597b9bd8d5711d7e19216e9f593baf
Marius <marinmariusdaniel02@gmail.com>
2022-05-22 13:33:14 +0300

    task2 refresh

--- tema3.c
+++ tema3.c
@@ -5,8 +5,13 @@
 
 int X = 0 ;
 
-void dfs2(float **matrix, int node, int dim, int *visited) {
+int Y = 0 ;
+
+int Z = 0 ;
+
+void dfs2(float **matrix, int node, int dim, int *visited, int afisare[20][20]) {
     
+    afisare[Y][Z] = node ;
     printf("%d ", node);
     visited[node] = 1;
  
@@ -14,13 +19,13 @@ void dfs2(float **matrix, int node, int dim, int *visited) {
     for (int i = 0; i < dim ; i++) {
         
         if (matrix[node][i] != 0 && (!visited[i])) {
-            dfs2(matrix, i, dim, visited);
+            Z++;
+            dfs2(matrix, i, dim, visited, afisare);
         }
     }
 
 
 }
-
 
 void dfs(float **matrix, int node, int dim, int *visited, int *stack) {
     
@@ -58,7 +63,6 @@ int delivery(float *distance, int *vis, int *traversal, int dim) {
     return index;
 }
 
- 
 void printSolution(float *dist, int dim) {
     printf("Vector \t\t Distanta\n");
     for (int i = 0; i < dim ; i++)
@@ -286,39 +290,46 @@ int main() {
           free(magazine);
         }
 
-         if(strstr("e2",arg) != NULL) {
+        if(strstr("e2",arg) != NULL) {
          
-         printf("depozit id : %d\n",depozite[0]);
-
-
+         // vector nod vizitat
          int *vector = (int *)malloc(number_nodes * sizeof(int));
+         // stiva ( sub forma de vector )
          int *stack = (int *)malloc(number_nodes * sizeof(int));
          for(int i = 0 ; i < number_nodes ; i++) {
              vector[i] = 0;
              stack[i] = -1;
          }
-
+         
+         // dfs din nodul 0
          dfs(matrix,0,number_nodes,vector,stack);
-
+         
+         // declarare matrice transpusa
          float **transpose = (float **)malloc(number_nodes * sizeof(float *));
          for(int i = 0 ; i < number_nodes ; i++) {
              transpose[i] = (float *)malloc(number_nodes * sizeof(float));
          }
-
+         
+         // transpunere matrice
          trans(matrix,transpose,number_nodes);
 
          print_matrix(matrix, number_nodes);
          printf("\n");
          print_matrix(transpose, number_nodes);
          
+         // pop la stack
          int *stack_trans = (int *)malloc(number_nodes * sizeof(int));
-          
+         
+         // refresh valori pentru alt dfs
          for(int i = 0 ; i < number_nodes ; i++) {
              vector[i] = 0;
              stack_trans[i] = stack[number_nodes - i - 1];
          }
-        vector[0] = 1;
-        vector[1] = 1;
+         
+         // scoatere depozite din componente tare conexe
+         for(int i = 0 ; i < number_dep ; i++)
+            vector[depozite[i]] = 1;
+
 
          for(int i = 0 ; i < number_nodes ; i++) {
              printf("order stack : %d\n", stack[i]);
@@ -328,12 +339,67 @@ int main() {
              printf("order reverse stack : %d\n", stack_trans[i]);
          }
          
+         // matrice afisare
+         int afisare[20][20] = {0} ;
+         
+         // apelare dfs2
          for(int i = 0 ; i< number_nodes ; i++) {
             if(vector[stack_trans[i]] == 0) {
-            dfs2(transpose, stack_trans[i], number_nodes, vector);
+            Z = 0;
+            dfs2(transpose, stack_trans[i], number_nodes, vector, afisare);
+            Y++;
             printf("\n");
             }
          }
+         
+         int change;
+         int sort;
+
+         do {
+         // sortare linii
+         sort = 1;
+         for(int i = 0 ; i < Y ; i++)
+          for(int j = 0 ; j < 14 ; j++) 
+            if(afisare[i][j] > afisare[i][j+1] && afisare[i][j] != 0 && afisare[i][j+1] != 0) {
+             change = afisare[i][j];
+             afisare[i][j] = afisare[i][j+1];
+             afisare[i][j+1] = change;
+             sort = 0;
+          }
+         
+          }
+          while(sort == 0);
+
+         
+         do {
+         // sortare first element
+
+         sort = 1;
+         for(int i = 0 ; i < Y - 1 ; i++)
+          if(afisare[i][0] > afisare [i+1][0]) {
+              for(int j = 0 ; j < 15 ; j++) {
+                  change = afisare[i+1][j];
+                  afisare[i+1][j] = afisare[i][j];
+                  afisare[i][j] = change;
+              }
+              sort = 0;
+          }
+          
+          }
+          while(sort == 0);
+         
+         // afisare
+         int cp_y = Y;
+         for(int i = 0 ; i < Y ; i++) {
+           for(int j = 0 ; j < 15 ; j++) {
+             if(afisare[i][j] != 0)
+             printf("%d ", afisare[i][j]);
+             if(cp_y > 1) {
+             printf("\n");
+             cp_y--;
+             }
+           }
+         }  
 
 
          for (int i = 0; i < number_nodes; i++)
